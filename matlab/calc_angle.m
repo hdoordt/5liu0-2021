0938f2dd01e3e689e4ceb6252287711b6b7a60bf
Henk Dieter Oordt <hd@oordt.dev>
2022-01-22 15:06:21 +0100

    base calc_angle matlab function on built in correlation method

--- matlab/calc_angle.m
+++ matlab/calc_angle.m
@@ -1,12 +1,11 @@
 function theta = calc_angle(ref, sig, Ts)
-    % corr = corr_samples(ref, sig, Ts);
-    % [~, argmax] = max(corr.corr);
-    % lag = corr.lags(argmax);
-    % tau = lag * Ts; 
-    c = xcorr_custom(ref, sig, Ts);
-    tau = c.tau
-    v = 343; % Speed of sound
-    d_mics = 0.125; % Distance between the two microphones
-    cos_theta = tau * v / d_mics
+    corr = corr_samples(ref, sig, Ts);
+    [~, argmax] = max(corr.corr);
+    lag = corr.lags(argmax);
+    tau = lag * Ts; 
+    
+    v = 343; % Speed of sound in m/s
+    d_mics = 0.125; % Distance between the two microphones in m
+    cos_theta = tau * v / d_mics;
     theta = acos(cos_theta);
 end
