6996b29c0f376239daa75d8541b8e8c2476cfa6d
Henk Dieter Oordt <hd@oordt.dev>
2022-02-01 11:05:18 +0100

    use corr_samples in calc_angle matlab function

--- matlab/calc_angle.m
+++ matlab/calc_angle.m
@@ -1,5 +1,5 @@
 function theta = calc_angle(ref, sig, Ts)
-    corr = corr_fft(ref, sig);
+    corr = corr_samples(ref, sig);
     [~, argmax] = max(corr.corr);
     lag = corr.lags(argmax);
     theta = lag_to_angle(lag, Ts, 0.125);
