f91c241e8dc67eec89b0d9530aa8e7cdccae0513
Henk Dieter Oordt <hd@oordt.dev>
2022-02-02 13:00:06 +0100

    add standard deviation to analysis

--- matlab/analyze_measurements.m
+++ matlab/analyze_measurements.m
@@ -1,9 +1,10 @@
 function [N, avg, ok_samples, pass_percentage, passed] = analyze_measurements(file_path, expected_angle) 
     [~, name, ext] = fileparts(file_path);
-    file_basename = sprintf("%s%s", name, ext);
+    file_basename = sprintf("%s%s", name, ext)
     samples = readmatrix(file_path);
     N = length(samples);
     avg = mean(samples);
+    std_dev = std(samples);
 
     ok_samples = 0;
     for x = samples'
@@ -18,8 +19,9 @@ function [N, avg, ok_samples, pass_percentage, passed] = analyze_measurements(fi
 
     fprintf("Number of samples:\t%d\n", N);
     fprintf("Average value:\t\t%g°\n", round(avg, 2))
+    fprintf("Standard deviation:\t%g\n", round(std_dev, 2))
     fprintf("Percent within range:\t%g%%\n", round(pass_percentage, 2));
     fprintf("Pass:\t\t\t%s\n", passed)
-    % file_basename & expected_angle & N & Avg & ok_samples & pass_percentage & test_passed
-    fprintf("%s & %g & %d & %g & %d & %g & %s \n",file_basename, expected_angle, N, avg, ok_samples, pass_percentage, passed);
+    % file_basename & expected_angle & N & Avg & stddev & ok_samples & pass_percentage & test_passed
+    fprintf("%s & %g & %d & %g & %g & %d & %g & %s \\\\\n",strrep(file_basename, '_', '\textunderscore '), expected_angle, N, round(avg,2), round(std_dev, 2), ok_samples, round(pass_percentage), passed);
 end
